3f97511c132fb2a56f692813d408cf424fcd6b93
root <root@montreal.localdomain>
2020-10-07 15:55:55 +0200

    start script added

--- client.py
+++ client.py
@@ -1,6 +1,22 @@
 import http.client
+from pynput import keyboard
 
-c = http.client.HTTPConnection('localhost', 8888)
-c.request('POST', '/process', '{"rara" : 3}')
-doc = c.getresponse().read()
-print(doc)
+client = http.client.HTTPConnection('localhost', 8888)
+
+def post_request(client, data) :
+	cient.request('POST', '/process', data)
+
+def on_press(key) :
+	print(key)
+	
+def on_release(key) :
+	print(key)
+
+
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener :
+	listener.join()
+
+#doc = c.getresponse().read()
+#print(doc)
