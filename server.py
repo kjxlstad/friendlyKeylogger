775e4c146bc8217555dcc75e368070c037dd21c2
root <root@montreal.localdomain>
2020-10-07 16:41:03 +0200

    server side with keymap and heatmap beginnings

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 import json
-
 from bottle import run, post, request, response
+
+keymap = [['',      'q',      'w',   'e',    'r', 't',     'y',     'u', 'i',    'o',    'p',  'backspace'],
+	      ['tab',   'a',      's',   'd',    'f', 'g',     'h',     'j', 'k',    'l',    ';',  '\''       ],   
+		  ['shift', 'z',      'x',   'c',    'v', 'b',     'n',     'm', ',',    '.',    '/',  'enter'    ],   
+          ['esc',   'ctrl_l', 'cmd', 'alt_l', '', 'space', 'space', '',  'left', 'down', 'up',  'right'   ]]   
+
+heatmap = [[0 for i in range(12)] for i in range(4)]
+#print(planck)
+
+#def update(key) :
+
+#def decipher(data) :
+	
+
 
 @post('/process')
 def my_process() :
