aea8534c6572a7bcf3dfe6f99a67b33764c95c3e
root <root@montreal.localdomain>
2020-10-07 16:19:49 +0200

    client side done

--- client.py
+++ client.py
@@ -4,19 +4,22 @@ from pynput import keyboard
 client = http.client.HTTPConnection('localhost', 8888)
 
 def post_request(client, data) :
-	cient.request('POST', '/process', data)
+	client.request('POST', '/process', data)
 
 def on_press(key) :
-	print(key)
-	
+	#post_request(client, '{"{0}" : 1}'.format(key))
+	post_request(client, '{"' + str(key) + '" : 1}') 
+	#post_request(client, '{"test" : 1}')	
+	check_response()
+
 def on_release(key) :
-	print(key)
+	#post_request(client, '{"{0}" : 0'.format(key))
+	post_request(client, '{"' + str(key) + '" : 0}')
+	#post_request(client, '{"test" : 0}')
+	check_response()
 
-
-
+def check_response() :
+	print(client.getresponse().read())
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener :
 	listener.join()
-
-#doc = c.getresponse().read()
-#print(doc)
