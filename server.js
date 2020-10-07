4e283a45d4eb11568f7fe73a73dbbbc942507b3c
root <root@montreal.localdomain>
2020-10-07 21:33:01 +0200

    new client

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
-const exec = require('child_process').exec
+const { exec } = require('child_process')
 const log = '/mnt/c/users/jonathan/documents/friendlyKeylogger/log.log'
 
+let a = 0
 fs.watch(log, (e, f) => {
-	exec(`tail -1 ${log} > ${log}`, (err, stdout, stderr) => {
-		console.log(stdout)
-	})
-}
+	if (a++ % 4 === 0) {
+		exec(`tail -1 ${log}`, (err, stdout, stderr) => {
+			console.log(stdout)
+		})
+	}
+})
