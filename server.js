d9a73ae0a87903098df4badc77f001c695a53a47
kjxlstad <jonathan.kjolstad@gmail.com>
2020-10-09 01:05:43 +0200

    Working blank blessed version

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const fs = require('fs')
 const { exec } = require('child_process')
-var Canvas = require('drawille')
+let Canvas = require('drawille')
 const line = require('bresenham')
-
+const blessed = require('blessed')
 const log = process.env.WINPATH
 
 const planck = {
@@ -16,12 +16,71 @@ const planck = {
 			  'esc',   'ctrl_l', 'cmd', 'alt_l', '',    'space', 'space', '',    'left', 'down', 'up', 'right'     ]
 }
 
-var c = new Canvas()
-const keyHeight = (c.height - 8)/ (planck.height)
-const keyWidth = keyHeight * 1.15
+let keys = []
+
+//var c = new Canvas()
+//const keyHeight = (c.height - 8)/ (planck.height)
+//const keyWidth = keyHeight * 1.15
 //const keyWidth = Math.min(c.width / (planck.width + 1), c.height / (planck.height + 1))
 //const keyHeight = keyWidth * 0.8
 
+let screen = blessed.screen({smartCSR: true, dockBorders: true})
+// Regular half window at current fontsize is 134 * 57
+
+
+/*
+var box = blessed.box({
+	top: 'center',
+	left: 'center',
+	width: '50%',
+	height: '50%',
+	content: 'Test {bold}Tester{/bold}',
+	border: {
+		type: 'line'
+	},
+	style: {
+		fg: 'white',
+		hover: {
+			bg: 'black'
+		}
+	}
+})
+*/
+
+
+// TODO calculate on the go, now based on 134 * 57 matrix
+const keyWidth = 10
+const keyHeight = 4
+
+for (let j = 0; j < 4; j++) {
+	for (let i = 0; i < 12; i++) {
+		const x = keyWidth * (i + 0.5) 
+		const y = keyHeight * (j + 0.25)
+		let key = blessed.box({
+			top: y,
+			left: x,
+			width: keyWidth + 1,
+			height: keyHeight + 1,
+			border: {
+				type: 'line'
+			},
+			style: {
+				fg: 'white'
+			}
+		})
+		keys.push(key)
+		screen.append(key)
+	}
+}
+
+// Quit on Escape, q, or Control-c
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+	return process.exit(0)
+})
+
+screen.render()
+
+/*
 function drawBox(x0, y0, x1, y1, filled) {
 	if (filled) for (let i = y0; i < y1; i++) line(x0, i, x1, i, c.set.bind(c))
 	else for (let i = y0; i < y1; i++) line(x0, i, x1, i, c.unset.bind(c))
@@ -58,13 +117,14 @@ function update(key, dir) {
 	const x = linIndex % planck.width
 	updateKey(x, y, dir)
 }
+*/
 
 let a = 0
 fs.watch(log, (e, f) => {
 	if (a++ % 4 === 0) {
 		exec(`tail -1 ${log}`, (err, stdout, stderr) => {
 			const [key, dir] = stdout.split(' ')
-			update(key, parseInt(dir))
+			//update(key, parseInt(dir))
 		})
 	}
 })
