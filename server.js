14436952c9d851b9931b0fcaa16b5a5ef7a920d8
kjxlstad <jonathan.kjolstad@gmail.com>
2020-10-09 01:07:35 +0200

    removed braille and bresenham dependency

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const fs = require('fs')
 const { exec } = require('child_process')
-let Canvas = require('drawille')
-const line = require('bresenham')
 const blessed = require('blessed')
 const log = process.env.WINPATH
 
@@ -17,12 +15,6 @@ const planck = {
 }
 
 let keys = []
-
-//var c = new Canvas()
-//const keyHeight = (c.height - 8)/ (planck.height)
-//const keyWidth = keyHeight * 1.15
-//const keyWidth = Math.min(c.width / (planck.width + 1), c.height / (planck.height + 1))
-//const keyHeight = keyWidth * 0.8
 
 let screen = blessed.screen({smartCSR: true, dockBorders: true})
 // Regular half window at current fontsize is 134 * 57
@@ -79,45 +71,6 @@ screen.key(['escape', 'q', 'C-c'], (ch, key) => {
 })
 
 screen.render()
-
-/*
-function drawBox(x0, y0, x1, y1, filled) {
-	if (filled) for (let i = y0; i < y1; i++) line(x0, i, x1, i, c.set.bind(c))
-	else for (let i = y0; i < y1; i++) line(x0, i, x1, i, c.unset.bind(c))
-	drawBorder()	
-	process.stdout.write(c.frame())
-}
-
-function drawBorder() {
-	// Vertical lines
-	line(0, 4, planck.width * keyWidth, 4, c.set.bind(c))
-	line(0, 4 + keyHeight * planck.height, planck.width * keyWidth, 4 + keyHeight * planck.height, c.set.bind(c))
-
-	// Horizontal
-	line(0, 4, 0, 4 + keyHeight * planck.height, c.set.bind(c))
-	line(planck.width * keyWidth, 4, planck.width * keyWidth, 4 + keyHeight * planck.height, c.set.bind(c))
-}
-
-function getPos(i, j) {
-	const x0 = keyWidth * i
-	const x1 = keyWidth * (i + 1)
-	const y0 = keyHeight * j + 4
-	const y1 = keyHeight * (j + 1) + 4
-	return [x0, y0, x1, y1]
-}
-
-function updateKey(i, j, filled) {
-	const pos = getPos(i, j)
-	drawBox(pos[0], pos[1], pos[2], pos[3], filled)
-}
-
-function update(key, dir) {
-	const linIndex = planck.keymap.findIndex((e) => e == key)
-	const y = Math.floor(linIndex / planck.width)
-	const x = linIndex % planck.width
-	updateKey(x, y, dir)
-}
-*/
 
 let a = 0
 fs.watch(log, (e, f) => {
