41f8fd017e0bcb754109275517c6be424424b8a5
kjxlstad <jonathan.kjolstad@gmail.com>
2020-10-09 01:51:40 +0200

    working version based on blsesed, goodnight

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const blessed = require('blessed')
 require('dotenv').config()
 const log = process.env.WINPATH
 
+// TODO add styling and track stats since boot
+
 // Current keyboard specifics TODO move to config file
 const planck = {
 	width: 12,
@@ -23,25 +25,26 @@ let keys = []
 let screen = blessed.screen({smartCSR: true, dockBorders: true})
 
 // TODO calculate on the go, now based on 134 * 57 matrix
-const keyWidth = 10
+const keyWidth = 9
 const keyHeight = 4
 
 for (let j = 0; j < 4; j++) {
 	for (let i = 0; i < 12; i++) {
-		const x = keyWidth * (i + 0.5) 
-		const y = keyHeight * (j + 0.25)
+		const x = (keyWidth + 1) * i + 5
+		const y = keyHeight * j + 1
+		
+		// Create and style box objects, one for each key
 		let key = blessed.box({
 			top: y,
 			left: x,
 			width: keyWidth + 1,
 			height: keyHeight + 1,
 			border: {
-				type: 'line'
-			},
-			style: {
-				fg: 'white'
+				type: 'bg',
+				bg: 'white',
 			}
 		})
+
 		keys.push(key)
 		screen.append(key)
 	}
@@ -52,12 +55,25 @@ screen.key(['escape', 'q', 'C-c'], (ch, key) => {
 	return process.exit(0)
 })
 
+function update(key, dir) {
+	if (!planck.keymap.includes(key)) return
+	let pressed = keys[planck.keymap.indexOf(key)]
+	if (dir) {
+		pressed.style.bg = 'white'
+		pressed.border.bg = 'white'
+	} else {
+		pressed.style.bg = 'none'
+		pressed.border.bg = 'none'
+	}
+	screen.render()
+}
+
 let a = 0
 fs.watch(log, (e, f) => {
 	if (a++ % 4 === 0) {
 		exec(`tail -1 ${log}`, (err, stdout, stderr) => {
 			const [key, dir] = stdout.split(' ')
-			//update(key, parseInt(dir))
+			update(key, parseInt(dir))
 		})
 	}
 })
