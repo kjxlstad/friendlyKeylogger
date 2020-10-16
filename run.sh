29ba8eb6e68d31311d509ad4393947030a87c34e
kjxlstad <jonathan.kjolstad@gmail.com>
2020-10-16 16:30:52 +0200

    newlines

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ fi
 dir=/mnt/c"$1"/friendlyKeyloggerClient
 
 if [[ -d "$dir" ]]; then
+	echo ""
 	echo "Folder $dir allready exists, do you want to overwrite it?"
 	select yn in "Yes" "No"; do
 		case $yn in
@@ -23,14 +24,16 @@ log="$dir"/log.log
 echo WINPATH="$log" > .env
 touch "$log"
 
+echo ""
 echo "Run client as windows admin now?"
 select yn in "Yes" "No"; do
 	case $yn in
 		Yes ) cd "$dir"/client; cmd.exe /c runElevatedClient.bat; break;;
-		No ) exit;;
+		No ) break;;
 	esac
 done
 
+echo ""
 echo "Run display host in this window?"
 select yn in "Yes" "No"; do
 	case $yn in
